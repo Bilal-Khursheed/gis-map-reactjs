fd808f6705adfb9abc7983f43344c696655ac325
bilalkhursheed <bilal.khursheed617@gmail.com>
2022-10-10 13:29:16 +0000

    new data

--- src/Components/Map.jsx
+++ src/Components/Map.jsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 import {
     withGoogleMap,
     GoogleMap,
     Marker,
-    Polygon
+    Polygon,
+    InfoWindow
 } from "react-google-maps";
+
 // const coords = [
 //     { lng: -73.94718884609291, lat: 40.73716326270996, },
 //     { lng: -73.94785154644617, lat: 40.73594880162498, },
@@ -17,27 +19,62 @@ import {
 //     { lng: -73.94718884609291, lat: 40.73716326270996 },
 // ];
 
-const MapWithAMarker = withGoogleMap(props =>
-    <GoogleMap
-        defaultZoom={8}
+const MapWithAMarker = withGoogleMap(({ centerPoints, mapMakers, polygonData }) => {
+    const [selectedElement, setSelectedElement] = useState(null);
+    const [activeMarker, setActiveMarker] = useState();
 
+    const mapRef = useRef(null);
+    console.log("here is reffff===>>>", mapRef.current)
+    // Fit bounds function
+    const fitBounds = () => {
+        const bounds = new window.google.maps.LatLngBounds();
+        mapMakers.map(item => {
+            bounds.extend(item.cordinates);
+            return item.id
+        });
+        mapRef.current.fitBounds(bounds);
+    };
+
+    // Fit bounds on mount, and when the markers change
+    useEffect(() => {
+        fitBounds();
+    }, [mapMakers]);
+
+    return <GoogleMap
+        defaultZoom={4}
+        ref={mapRef}
         // defaultCenter={{ lng: props?.centerPoints && parseInt(props?.centerPoints[0]), lat: props?.centerPoints && parseInt(props?.centerPoints[1]) }}
         // defaultCenter={{ lng: -73.93279080176559, lat: 40.727263865864586 }}
-        defaultCenter={props.centerPoints}
+        defaultCenter={centerPoints}
     >
         {
-            props && props.mapMakers?.map((item, ind) => {
+            mapMakers?.map((item, ind) => {
                 return (
                     <Marker
+
                         key={ind}
                         // position={{ lng: mapPoints?.coordinates[0], lat: mapPoints?.coordinates[1] }}
-                        position={item}
-                    />
+                        position={item?.cordinates}
+                        onClick={() => {
+                            setSelectedElement(item);
+                            setActiveMarker(ind);
+                        }
+                        }
+                    >
+                        {activeMarker === ind ? (
+                            <InfoWindow onCloseClick={() => setActiveMarker(null)} key={ind + 1}>
+                                <div><h3>{selectedElement?.region}</h3>
+                                    <label>Address: {selectedElement?.address}</label>
+                                    <span>Postal Code: {selectedElement?.postcode}</span>
+                                </div>
+
+                            </InfoWindow>
+                        ) : null}
+                    </Marker>
                 )
             })
         }
-
-        {props?.polygonData?.map((item, ind) => (
+        {polygonData?.map((item, ind) => (
             <>
                 {/* <Marker
                     key={ind}
@@ -60,8 +97,10 @@ const MapWithAMarker = withGoogleMap(props =>
 
         }
     </GoogleMap>
+}
 );
 const MapChart = ({ mapData, centerPoints, mapMakers, polygonData }) => {
+
     return (
         <MapWithAMarker
             containerElement={<div style={{ height: `100vh` }} />}
@@ -69,6 +108,7 @@ const MapChart = ({ mapData, centerPoints, mapMakers, polygonData }) => {
             mapData={mapData}
             centerPoints={centerPoints}
             mapMakers={mapMakers}
+            onMapIdle={() => { console.log('map is ready') }}
             polygonData={polygonData}
         />
     );
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -7,33 +7,48 @@ const HomePage = () => {
     const [mapData, setMapData] = useState();
     const [centerPoints, setCenterPoints] = useState();
     const [mapMakers, setMapMakers] = useState();
-    const [polygonData, setPolygonData] = useState();
+    const [polygonData, setPolygonData] = useState([]);
+    const [limit, setLimit] = useState(1000);
+    const [offset, setOffset] = useState(0);
+    const [totalCount, setTotalCount] = useState(0)
     useEffect(() => {
-        const fetchMaps = async () => {
-            try {
-                let url = FETCH_MAP_DATA;
-                let init = {
-                    method: "GET",
-                    headers: {
-                        Accept: "application/json",
-                        "Content-Type": "application/json",
-                    },
-                };
-                let response = await sendRequest(url, init);
-                if (response && response.success) {
-                    response = response.data
-                    setCenterPoints(response?.pointData?.centerPoints);
-                    setMapMakers(response?.pointData?.mapPoints);
-                    setMapData(response);
-                    setPolygonData(response?.polygonData)
-                }
+        fetchMaps(limit, offset)
+    }, []);
 
-            } catch (error) {
-                console.log("ERROR while fetching maps===>", error);
+
+
+
+    const fetchMaps = async (limit, offset) => {
+        try {
+            let url = FETCH_MAP_DATA + `?limit=${limit}&offset=${offset}`;
+            let init = {
+                method: "GET",
+                headers: {
+                    Accept: "application/json",
+                    "Content-Type": "application/json",
+                },
+            };
+            let response = await sendRequest(url, init);
+            if (response && response.success) {
+                response = response.data
+                setCenterPoints(response?.pointData?.centerPoints);
+                setMapMakers(response?.pointData?.mapPoints);
+                setMapData(response);
+                setPolygonData((recent) => [...response?.polygonData?.polygons, ...recent]);
+                setTotalCount(response?.polygonData?.totalCount);
+                if (offset < response?.polygonData?.totalCount) {
+                    setTimeout(() => {
+                        fetchMaps(limit, (offset + limit))
+                    }, 5000);
+
+                }
             }
+
+        } catch (error) {
+            console.log("ERROR while fetching maps===>", error);
         }
-        fetchMaps()
-    }, [])
+    }
+    console.log("here is data===>>>", polygonData)
 
     return (<>
         {mapData && mapMakers && centerPoints ? <MapComponent mapData={mapData} mapMakers={mapMakers} centerPoints={centerPoints} polygonData={polygonData} /> : null}
