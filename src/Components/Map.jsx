9f20f4cc661c8e1a791de2c0ca158a1b74f668c8
bilalkhursheed <bilal.khursheed617@gmail.com>
2022-10-11 12:22:51 +0000

    clean code

--- src/Components/Map.jsx
+++ src/Components/Map.jsx
@@ -1,24 +1,8 @@
-import React, { createRef, useCallback, useEffect, useRef, useState } from "react";
+import React, { createRef, useCallback, useState } from "react";
 import _debounce from 'lodash/debounce';
 
-import {
-    withGoogleMap,
-    GoogleMap,
-    Marker,
-    Polygon,
-    InfoWindow
-} from "react-google-maps";
+import { withGoogleMap, GoogleMap, Marker, Polygon, InfoWindow } from "react-google-maps";
 
-// const coords = [
-//     { lng: -73.94718884609291, lat: 40.73716326270996, },
-//     { lng: -73.94785154644617, lat: 40.73594880162498, },
-//     { lng: -73.94875595832566, lat: 40.73623377103011, },
-//     { lng: -73.9487061704137, lat: 40.73632501517496, },
-//     { lng: -73.9480840578246, lat: 40.737465122606544, },
-//     { lng: -73.94754222902405, lat: 40.737278565877524, },
-//     { lng: -73.9472009458731, lat: 40.73716651702501, },
-//     { lng: -73.94718884609291, lat: 40.73716326270996 },
-// ];
 
 const MapWithAMarker = withGoogleMap(({ centerPoints, mapMakers, polygonData, setLatNorth, setLatSouth, setLngEast, setLngWest }) => {
     const [selectedElement, setSelectedElement] = useState(null);
@@ -26,43 +10,14 @@ const MapWithAMarker = withGoogleMap(({ centerPoints, mapMakers, polygonData, se
 
 
     let mapRef = createRef(null);
-    // console.log("here is reffff===>>>",bounds)
-    // Fit bounds function
-    // const fitBounds = () => {
-    //     const bounds = new window.google.maps.LatLngBounds();
-    //     mapMakers.map(item => {
-    //         bounds.extend(item.cordinates);
-    //         return item.id
-    //     });
-    //     mapRef.current.fitBounds(bounds);
-    // };
-
-    // Fit bounds on mount, and when the markers change
-    // useEffect(() => {
-    //     fitBounds();
-    // }, [mapMakers]);
-    // useEffect(() => {
-    //     console.log(mapRef, "mapref-=====")
-    //     if (mapRef && mapRef.current !== null && typeof mapRef?.getBounds !== "undefined") {
-    //         setLatNorth(mapRef.getBounds()?.getNorthEast().lat());
-    //         setLatSouth(mapRef.getBounds()?.getSouthWest().lat());
-    //         setLngEast(mapRef.getBounds()?.getNorthEast().lng());
-    //         setLngWest(mapRef.getBounds()?.getSouthWest().lng())
-    //     }
-    // }, [mapRef]);
     const handleDebounceFn = (mapRef) => {
-        console.log("calling debounce fn====>>>")
-        // if (mapRef && mapRef.current !== null && typeof mapRef?.getBounds !== "undefined") {
         setLatNorth(mapRef?.getBounds()?.getNorthEast().lat());
         setLatSouth(mapRef?.getBounds()?.getSouthWest().lat());
         setLngEast(mapRef?.getBounds()?.getNorthEast().lng());
         setLngWest(mapRef?.getBounds()?.getSouthWest().lng())
-        // }
     };
+     // eslint-disable-next-line
     const handleBoundsChange = useCallback(_debounce(handleDebounceFn, 1000), []);
-
-
-
 
     return <GoogleMap
         defaultZoom={17}
@@ -73,8 +28,6 @@ const MapWithAMarker = withGoogleMap(({ centerPoints, mapMakers, polygonData, se
         onBoundsChanged={() => {
             if (mapRef && mapRef.current !== null && typeof mapRef?.getBounds !== "undefined") handleBoundsChange(mapRef)
         }}
-        // defaultCenter={{ lng: props?.centerPoints && parseInt(props?.centerPoints[0]), lat: props?.centerPoints && parseInt(props?.centerPoints[1]) }}
-        // defaultCenter={{ lng: -73.93279080176559, lat: 40.727263865864586 }}
         defaultCenter={centerPoints}
     >
         {
@@ -83,7 +36,6 @@ const MapWithAMarker = withGoogleMap(({ centerPoints, mapMakers, polygonData, se
                     <Marker
 
                         key={ind}
-                        // position={{ lng: mapPoints?.coordinates[0], lat: mapPoints?.coordinates[1] }}
                         position={item?.cordinates}
                         onClick={() => {
                             setSelectedElement(item);
@@ -105,23 +57,16 @@ const MapWithAMarker = withGoogleMap(({ centerPoints, mapMakers, polygonData, se
             })
         }
         {polygonData?.map((item, ind) => (
-            <>
-                {/* <Marker
-                    key={ind}
-                    // position={{ lng: mapPoints?.coordinates[0], lat: mapPoints?.coordinates[1] }}
-                    position={item?.coordinates[0]}
-                /> */}
-                <Polygon
-                    path={item?.coordinates}
-                    key={ind + 1}
-                    options={{
-                        fillColor: "red",
-                        fillOpacity: 0.4,
-                        strokeColor: "#000",
-                        strokeOpacity: 1,
-                        strokeWeight: 1
-                    }} />
-            </>
+            <Polygon
+                path={item?.coordinates}
+                key={ind + 1}
+                options={{
+                    fillColor: "red",
+                    fillOpacity: 0.4,
+                    strokeColor: "#000",
+                    strokeOpacity: 1,
+                    strokeWeight: 1
+                }} />
 
         ))
 
@@ -129,7 +74,7 @@ const MapWithAMarker = withGoogleMap(({ centerPoints, mapMakers, polygonData, se
     </GoogleMap>
 }
 );
-const MapChart = ({ mapData, centerPoints, mapMakers, polygonData, setLatNorth, setLatSouth, setLngEast, setLngWest }) => {
+const MapChart = ({ centerPoints, mapMakers, polygonData, setLatNorth, setLatSouth, setLngEast, setLngWest }) => {
 
     return (
         <MapWithAMarker
@@ -139,10 +84,8 @@ const MapChart = ({ mapData, centerPoints, mapMakers, polygonData, setLatNorth, 
             setLngWest={setLngWest}
             containerElement={<div style={{ height: `100vh` }} />}
             mapElement={<div style={{ height: `100%` }} />}
-            mapData={mapData}
             centerPoints={centerPoints}
             mapMakers={mapMakers}
-            onMapIdle={() => { console.log('map is ready') }}
             polygonData={polygonData}
         />
     );
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,25 +4,17 @@ import { FETCH_MAP_DATA } from '../Settings/Constant';
 import { sendRequest } from '../Utils/apiService';
 
 const HomePage = () => {
-    const [mapData, setMapData] = useState();
     const [centerPoints, setCenterPoints] = useState();
     const [mapMakers, setMapMakers] = useState();
     const [polygonData, setPolygonData] = useState([]);
-    const [limit, setLimit] = useState(1000);
-    const [offset, setOffset] = useState(0);
-    // -73.97395491223583,40.650719856408784,-74.02588247876415,40.62101884532627,
-    const [lngWest, setLngWest] = useState(-73.97395491223583);
-    const [latSouth, setLatSouth] = useState(40.650719856408784);
-    const [lngEast, setLngEast] = useState(-74.02588247876415);
-    const [latNorth, setLatNorth] = useState(40.62101884532627);
-    const [totalCount, setTotalCount] = useState(0)
+    // -73.98442970695503 40.633985690721445 -73.97144781532295 40.64141074027972
+    const [lngWest, setLngWest] = useState(-73.98442970695503);
+    const [latSouth, setLatSouth] = useState(40.633985690721445);
+    const [lngEast, setLngEast] = useState(-73.97144781532295);
+    const [latNorth, setLatNorth] = useState(40.64141074027972);
     useEffect(() => {
         fetchMaps(lngWest, latSouth, lngEast, latNorth)
     }, [lngWest, latSouth, lngEast, latNorth]);
-
-    console.log("Lat lng=====>>>", lngWest, latSouth, lngEast, latNorth, totalCount)
-
-
     const fetchMaps = async (lngWest, latSouth, lngEast, latNorth) => {
         try {
             let url = FETCH_MAP_DATA + `?lngWest=${lngWest}&latSouth=${latSouth}&lngEast=${lngEast}&latNorth=${latNorth}`;
@@ -38,25 +30,15 @@ const HomePage = () => {
                 response = response.data
                 setCenterPoints(response?.pointData?.centerPoints);
                 setMapMakers(response?.pointData?.mapPoints);
-                setMapData(response);
                 setPolygonData(response?.polygonData?.polygons);
-                setTotalCount(response?.polygonData?.totalCount);
-                // if (offset < response?.polygonData?.totalCount) {
-                //     setTimeout(() => {
-                //         fetchMaps(limit, (offset + limit))
-                //     }, 5000);
-
-                // }
             }
 
         } catch (error) {
             console.log("ERROR while fetching maps===>", error);
         }
     }
-    console.log("here is data===>>>", polygonData)
-
     return (<>
-        {mapData && mapMakers && centerPoints ? <MapComponent setLatNorth={setLatNorth} setLatSouth={setLatSouth} setLngEast={setLngEast} setLngWest={setLngWest} mapData={mapData} mapMakers={mapMakers} centerPoints={centerPoints} polygonData={polygonData} /> : null}
+        {mapMakers && centerPoints && polygonData ? <MapComponent setLatNorth={setLatNorth} setLatSouth={setLatSouth} setLngEast={setLngEast} setLngWest={setLngWest} mapMakers={mapMakers} centerPoints={centerPoints} polygonData={polygonData} /> : null}
     </>
     )
 }
